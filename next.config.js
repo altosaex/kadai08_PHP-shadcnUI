ab266693d96ef4b9ac08b8ea26f518fce4ec251a
altosaex <147964457+altosaex@users.noreply.github.com>
2023-12-21 22:15:04 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com'], // 画像を読み込むホスト名を指定します
+  },
+}
+
+
+
